91b04ec53524905e767f327774b3b4721be0db0a
Walton Lee <mr.waltonlee@me.com>
2017-03-24 22:00:31 +0000

    Creating a file in the tmp directory

---
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Creating a file in tmp directory
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
