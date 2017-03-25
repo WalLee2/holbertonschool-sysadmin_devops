749f7688b9c4be2d05f98bc9fdd7b02401b739e7
Walton Lee <mr.waltonlee@me.com>
2017-03-25 05:03:05 +0000

    fixing the correct directory

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # debugging wordpress
 exec { 'Fix Wordpress':
   path    => '/sbin:/bin:/usr/sbin:/usr/bin',
-  command => "sed -i 's/phpp/php/'  /var/www/html/class-wp-locale.php"
+  command => "sed -i 's/phpp/php/'  /var/www/html/wp-settings.php"
 }
