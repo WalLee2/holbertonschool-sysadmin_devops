66a5b132535072a7b92b23bab1027bc52f6b7c0b
Walton Lee <mr.waltonlee@me.com>
2017-04-18 00:25:14 +0000

    converting to csv format

---
+++ 0x17-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Gather data from an API that uses REST API
+"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    usr = requests.get(user).json()
+    todo_list = 'https://jsonplaceholder.typicode.com/todos'
+    todo = requests.get(todo_list).json()
+    csv_file = open(sys.argv[1] + '.csv', 'w')
+    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    USERNAME = usr.get("username")
+    for a in todo:
+        if emp_id == str(a.get("userId")):
+            T_C_S = a.get("completed")
+            T_Title = a.get("title")
+            wr.writerow([emp_id, USERNAME, T_C_S, T_Title])
