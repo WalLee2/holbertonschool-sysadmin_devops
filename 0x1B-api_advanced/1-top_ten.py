dd5896fc552a12417c01f01fd2d7f3a2ae40c623
Walton Lee <mr.waltonlee@me.com>
2017-05-26 19:59:21 +0000

    Query the Reddit API and return the top 10 hottest posts

---
+++ 0x1B-api_advanced/1-top_ten.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import requests
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    req = requests.get(url, headers={'User-Agent': 'h'})
+    if (req.status_code == requests.codes.ok):
+        big_json = req.json()
+        for i in range(len(big_json['data']['children'])):
+            print(big_json['data']['children'][i]['data']['title'])
+    print(None)
