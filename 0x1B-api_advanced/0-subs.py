33cc618870091d1f3665e8a5ffee10b1b4e9cef9
Walton Lee <mr.waltonlee@me.com>
2017-05-26 19:58:40 +0000

    Querying the Reddit API for number of subscribers

---
+++ 0x1B-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Subroutine that grabs the number of subscribers
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Query API and return the number of subscribers if it exists
+    Return 0 if API does not exist
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url, headers={'User-Agent': 'lee'})
+    if (req.status_code == requests.codes.ok):
+        big_json = req.json()
+        return(big_json['data']['subscribers'])
+    return (0)
