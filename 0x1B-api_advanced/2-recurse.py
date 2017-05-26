f195ea37c4a5b6f90deeb3f4ecc370e72a86e648
Walton Lee <mr.waltonlee@me.com>
2017-05-26 23:17:35 +0000

    Count all the hot posts on reddit

---
+++ 0x1B-api_advanced/2-recurse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import requests
+
+def recurse(subreddit, host_list=[], next_str="0"):
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if not host_list:
+        req = requests.get(url, headers={'User-Agent': 'h_reddit'})
+    else:
+        if next_str is not None:
+            more = {'after': next_str}
+            req = requests.get(url, headers={'User-Agent': 'L'}, params=more)
+        else:
+            return (host_list)
+    big_json = req.json()
+    for i in range(len(big_json['data']['children'])):
+        host_list.append(big_json['data']['children'][i]['data']['title'])
+    return recurse(subreddit, host_list, big_json['data']['after'])
