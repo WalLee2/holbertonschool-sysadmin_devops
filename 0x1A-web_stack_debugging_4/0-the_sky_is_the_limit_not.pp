9d49383b801160fe649290360cb8ed95a6adde0e
Walton Lee <mr.waltonlee@me.com>
2017-05-27 04:47:22 +0000

    Fix puppet lint

--- 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Modify /etc/default/nginx to be able to close more requests
 # restart nginx after file has been changed
-exec { "restart nginx":
-     command   => "/bin/sed -i 's|-n 15|-n 4096|g' /etc/default/nginx ; /etc/init.d/nginx restart"
+exec { 'restart nginx':
+  command   => "/bin/sed -i 's|-n 15|-n 4096|g' /etc/default/nginx ; /etc/init.d/nginx restart"
 }
