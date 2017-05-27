5615e89fc76eef078c1c60d791aad7f80aca8df6
Walton Lee <mr.waltonlee@me.com>
2017-05-27 04:43:41 +0000

    Fix puppet file

--- 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,3 @@
-exec { 'modify_file':
-  path    => '/bin:',
-  command => "sed -i 's|-n 15|-n 4096|g' /etc/default/nginx"
-}
-exec { 'restart nginx':
-  command   => '/etc/init.d/nginx restart'
+exec { "restart nginx":
+     command   => "/bin/sed -i 's|-n 15|-n 4096|g' /etc/default/nginx ; /etc/init.d/nginx restart"
 }
